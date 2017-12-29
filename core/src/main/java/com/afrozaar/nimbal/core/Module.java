e1fa296a56b61f16561470e752f75a770eebbc92
Michael Wiles <michael.wiles@afrozaar.com>
2017-12-29 20:11:59 +0200

    added copyright

--- core/src/main/java/com/afrozaar/nimbal/core/Module.java
+++ core/src/main/java/com/afrozaar/nimbal/core/Module.java
@@ -1,3 +1,10 @@
+/*******************************************************************************
+ * Nimbal Module Manager 
+ * Copyright (c) 2017 Afrozaar.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License 2.0
+ * which accompanies this distribution and is available at https://www.apache.org/licenses/LICENSE-2.0
+ *******************************************************************************/
 package com.afrozaar.nimbal.core;
 
 import static java.util.stream.Collectors.joining;
